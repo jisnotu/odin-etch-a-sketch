d1a2f74c039bff8dd0508c765d739ced24730fbc
jisnotu <133517588+jisnotu@users.noreply.github.com>
2025-02-12 19:16:16 -0500

    Add 16 divs inside container div

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  const square = document.createElement("div");
+  square.classList.toggle("square");
+  container.appendChild(square);
+}
