a5d272c3e284959438aaae58501704063933545b
jisnotu <133517588+jisnotu@users.noreply.github.com>
2025-02-14 18:13:28 -0500

    Create 16x16 grid of squares in container div
    Made squares red with black outline to clearly show squares while
    testing. Outline, unlike border, does not take up space so it will
    not mess up dimensions.
    
    Used flex-wrap to get grid layout as the assignment calls for using
    flexbox to achieve the grid layout.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < (16 * 16); i++) {
   const square = document.createElement("div");
   square.classList.toggle("square");
   container.appendChild(square);
